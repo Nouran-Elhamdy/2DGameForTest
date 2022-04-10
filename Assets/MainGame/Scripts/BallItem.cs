5a6636e642d90262cd1d7cba0960f6311fba3bfa
NouranElhamdy <nouranelhamdy@gmail.com>
2022-04-10 04:27:04 +0200

    update projectile for donut

--- Assets/MainGame/Scripts/BallItem.cs
+++ Assets/MainGame/Scripts/BallItem.cs
@@ -1,5 +1,6 @@
 using Platformer.Mechanics;
 using UnityEngine;
+using Test.Manager;
 public class BallItem : MonoBehaviour
 {
     #region Public Variables
@@ -10,6 +11,7 @@ public class BallItem : MonoBehaviour
     #region Private Variables
     Vector3 rocketPosition;
     SpriteRenderer spriteRenderer;
+
     float newX;
     float newY;
     #endregion
@@ -22,37 +24,62 @@ public class BallItem : MonoBehaviour
     #endregion
 
     #region Unity Calls
-    private void OnEnable()
+    private void Start()
     {
         spriteRenderer = FindObjectOfType<PlayerController>().GetComponent<SpriteRenderer>();
         transform.localScale = new Vector3(0.5f, 0.5f, 0);
-        transform.localPosition = new Vector3();
-        rocketPosition = new Vector3();
+
+
+        if (currentBallConfig.ballType == BallType.EnergyBall)
+        {
+            if (!spriteRenderer.flipX)
+                GetComponent<Rigidbody2D>().velocity = transform.right * 10;
+
+            else
+                GetComponent<Rigidbody2D>().velocity = -transform.right * 10;
+        }
+        else
+        {
+            if (!spriteRenderer.flipX)
+            {
+                transform.localPosition = new Vector3();
+                rocketPosition = new Vector3();
+            }
+            else
+            {
+                transform.localPosition = new Vector3();
+                rocketPosition = new Vector3();
+            }
+        }
     }
     void Update()
     {
-        if (!spriteRenderer.flipX)
+        if (currentBallConfig.ballType != BallType.EnergyBall)
         {
-            newX = rocketPosition.x + speed;
-            newY = GetY(newX, !spriteRenderer.flipX);
+            if (!spriteRenderer.flipX)
+            {
+                newX = rocketPosition.x + speed;
+                newY = GetY(newX, true);
+            }
+            else
+            {
+                newX = rocketPosition.x - speed;
+                newY = GetY(newX, false);
+            }
+            rocketPosition = new Vector3(newX, newY, 0);
+            transform.localPosition = rocketPosition;
         }
-        else 
-        { 
-            newX = rocketPosition.x - speed;
-            newY = GetY(newX, false);
-        }
-
-        rocketPosition = new Vector3(newX, newY, 0);
-        transform.localPosition = rocketPosition;
     }
     #endregion
 
     #region Private Methods
     float GetY(float x, bool isFlipped)
     {
-        if(isFlipped) 
-            return (-0.08f * Mathf.Pow(x, 2)) + 0.8f * x;
-        else 
+        //if (isFlipped)
+        //    return (-0.08f * Mathf.Pow(x, 2)) + 0.8f * x;
+        if (isFlipped)
+            return (-0.08f * Mathf.Pow(x, 2)) + (1.76f * x) - 7.68f;
+        else
             return (-0.08f * Mathf.Pow(x, 2)) - 0.8f * x;
     }
     #endregion
