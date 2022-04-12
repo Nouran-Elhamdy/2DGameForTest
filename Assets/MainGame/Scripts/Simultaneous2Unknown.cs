8489599240b69e4c97fafe96d796e9f12a93e411
NouranElhamdy <nouranelhamdy@gmail.com>
2022-04-12 05:14:03 +0200

    Update Simultaneous2Unknown.cs

--- Assets/MainGame/Scripts/Simultaneous2Unknown.cs
+++ Assets/MainGame/Scripts/Simultaneous2Unknown.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
   class Simultaneous2Unknown
     {
         private double[] x_coefficients;
@@ -22,18 +18,15 @@ using UnityEngine;
 
         public double[] SolveSimultaneous()
         {
-            // STEP 2:
             eliminator[0, 0] = y_coefficients[1] * x_coefficients[0];
             eliminator[0, 1] = y_coefficients[1] * equals[0];
-            // STEP 3:
+
             eliminator[1, 0] = y_coefficients[0] * x_coefficients[1];
             eliminator[1, 1] = y_coefficients[0] * equals[1];
 
             try
             {
-                // STEPS 4, 5:
                 x_variable = (eliminator[0, 1] - eliminator[1, 1]) / (eliminator[0, 0] - eliminator[1, 0]);
-                // STEP 6:
                 y_variable = (equals[0] - x_coefficients[0] * x_variable) / y_coefficients[0];
             }
             catch (Exception e)
